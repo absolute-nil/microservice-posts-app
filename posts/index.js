f564d3bd14ec447152dccec1666ed4eaee435203
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-22 15:35:58 +0530

    create posts server

--- posts/index.js
+++ posts/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const crypto = require("crypto");
+const cors = require("cors")
 
-const app = express()
+const app = express();
 
-app.get('/posts', (req,res) =>{});
+app.use(express.json());
+app.use(cors())
 
-app.post('/posts', (req,res) => {});
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.status(200).send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = crypto.randomBytes(4).toString("hex");
+  const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
-    console.log("listening on port 4000");
-})
+  console.log("listening on port 4000");
+});
