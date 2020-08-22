6a7b7f9f8cd34f55a0e6276953c832c65ed2aa20
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-08-23 02:14:43 +0530

    handle comment status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,9 +3,26 @@ import React from "react";
 const CommentList = ({ comments }) => {
 
   const renderedComments = comments.map((comment) => {
+
+    let content;
+
+    switch (comment.status) {
+      case "pending":
+        content = "This comment is pending approval"
+        break;
+        case "approved":
+          content = comment.content
+          break;
+        case "rejected":
+          content = "This comment has been rejected"
+          break;
+      default:
+        break;
+    }
+
     return (
       <div className="item" key={comment.id}>
-        {comment.content}
+        {content}
       </div>
     );
   });
